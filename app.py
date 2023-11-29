b610aeea543a40c8daa2db53b4cca33089673e5b
saulosgil <saulosgil@hotmail.com>
2023-11-29 09:26:04 -0300

    variaveis-modulo2-aula1

---
+++ app.py
@@ -0,0 +1,51 @@
+# Exemplo de variavel 
+velocidade_internet = 10
+print(velocidade_internet)
+
+# Quais tipo de variáveis podemos armazenar na memória do computador?
+# números
+velocidade_internet
+print(f'O tipo da variável velocidade da internet é {type(velocidade_internet)}.')
+
+# valores boleanos
+loja_esta_aberta = True # or False
+loja_esta_aberta
+print(f'O tipo da variável loja_esta_abert é {type(loja_esta_aberta)}.')
+
+# strings
+slogan = 'Feito é melhor que perfeito!' # aspas simples
+slogan = "Feito é melhor que perfeito!" # aspas duplas
+slogan
+print(f'O tipo da variável slogan é {type(slogan)}.')
+
+# Packing
+a,b,c,d = 1,2,3,4
+print(a)
+print(b)
+print(c)
+print(d)
+
+# DESAFIO
+# Copie e cole as seguintes lçinhas de código para seu editor de código e descubra
+# qual o tipo de cada uma das variáveis.
+
+variavel_1 = 25.87
+variavel_2 = True
+variavel_3 = 'Bom dia!'
+variavel_4 = 15
+
+# resposta 1
+print(f'A variável_1 é do tipo é {type(variavel_1)};')
+
+# resposta 2
+print(f'A variável_2 é do tipo é {type(variavel_2)};')
+
+# resposta 3
+print(f'A variável_3 é do tipo é {type(variavel_3)};')
+
+# resposta 4
+print(f'A variável_4 é do tipo é{type(variavel_4)};')
+
+
+
+
