fd04b81cbaf75deb0cd13195561c4d9cd39a26a6
saulosgil <saulosgil@hotmail.com>
2023-12-12 10:57:23 -0300

    while

--- app.py
+++ app.py
@@ -655,3 +655,67 @@ versoes = ['Plus', 'Premium Plus', 'Premium Deluxe', 'Plus Premium Ultra']
 for marca in celulares:
     for versao in versoes:
         print(f'{marca} {versao}')
+
+# LOOP WHILE
+tentativas = 0
+
+while tentativas < 3:
+    print('Tente novamente')
+    tentativas += 1
+
+# senha
+senha = ''
+
+while senha != '123456':
+    senha = input('Digite sua senha')
+    print('Bem-vindo!')
+
+# Exemplo
+# Obrigar a pessoa a digitar algo
+nome = ''
+
+while nome == '':
+    nome = input('Digite seu nome: ')
+    print(f'Bem-vindo {nome}')
+
+# Exemplo 2
+horario = 0
+
+while horario <= 17:
+    print(horario)
+    horario += 1
+    print('Hora de ir ver o por do sol')
+
+# Exemplo 3 -  contador regressiva
+contador = 100
+
+while contador >= 0:
+    print(contador)
+    contador -= 1
+
+# DESAFIOS🥇
+# DESAFIO 1 - Crie um loop while que irá contar e imprimir no console de 1 até 120
+numero = 1
+
+while numero < 121:
+    print(numero)
+    numero += 1
+
+# DESAFIO 2 - Crie um loop while que irá continuamente pedir ao usuário a senha 
+# para entrada, e só irá permitir o programa continuar caso ele digite a senha 'secreto'
+senha = ''
+
+while senha != 'secreto':
+    senha = input('Digite sua senha')
+
+# DESAFIO 3 - Crie um loop que conte e imprima na tela o valor em ordem 
+# descrescente de 100 para 1
+contador = 100
+
+while contador >= 0:
+    print(contador)
+    contador -= 1
+
+
+
+
