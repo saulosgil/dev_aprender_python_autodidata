a2ae894930f50a9b60f084eb21fb543d0e696b41
saulosgil <saulosgil@hotmail.com>
2023-12-12 08:02:42 -0300

    condicionais

--- app.py
+++ app.py
@@ -478,3 +478,96 @@ tuple()
 list()
 '''
 
+# IF AND ELSE
+trabalho_terminado = False
+
+if trabalho_terminado == True:
+    print('Bora dar uma saida!')
+else: print('Não posso sair agora!')
+
+# Exemplo 1
+numero_atrasos = 2
+
+if numero_atrasos >= 3:
+    print('Vá para a diretoria')
+elif numero_atrasos == 2:
+    print('Essa é sua segunda falta')
+elif numero_atrasos == 1:
+    print('Essa é sua primeira falta')
+else:
+    print('Pode entrar')
+
+'''
+A velocidade máxima para essa rua é 50km
+    * cruzou entre 50 a 60km, levou multa de 2 pontos
+    * cruzou entre 61 a 75km, levou multa de 3 pontos
+    * cruzou acima de 75km, levou multa de 7 pontos
+'''
+velocidade = 100
+
+if velocidade <= 50:
+    print('Não foi multado')
+elif velocidade >= 50 and velocidade <= 60:
+    print('Levou multa de 2 pontos')
+elif velocidade >= 61 and velocidade <= 75:
+    print('Levou multa de 2 pontos')
+else:
+    print('Levou multa de 7 pontos')
+
+# Chaining
+if velocidade <= 50:
+    print('Não foi multado')
+# elif velocidade >= 50 and velocidade <= 60:
+elif 51 <= velocidade <= 60:
+    print('Levou multa de 2 pontos')
+# elif velocidade >= 61 and velocidade <= 75:
+elif 61 >= velocidade <=75:
+    print('Levou multa de 2 pontos')
+else:
+    print('Levou multa de 7 pontos')
+
+# Desafio 🥇
+
+# Monte o seguinte cenário usando condicionais
+
+# Você está montando um sistema para um salão de beleza para calcular o preço do 
+# corte de cabelos grandes que irá seguir as seguinte regras
+
+'''
+Se seu cabelo estiver com ou abaixo de 20cm você paga o valor de R$50,00
+
+Se seu cabelo estiver entre 21cm a 30cm você paga o valor de R$70,00
+
+Se seu cabelo estiver entre 31cm a 50cm você paga o valor de R$100,00
+
+Acima de 50cm Favor consultar na recepção
+
+# Declare uma variável que represente o tamanho atual do cabelo
+
+# Apenas imprima na tela a mensagem apropriada, nada além disso é necesário
+'''
+tamanho_cabelo = 100
+
+if tamanho_cabelo <= 20:
+    print('O valor é R$50,00')
+elif tamanho_cabelo >= 21 and tamanho_cabelo <= 30:
+    print('O valor é R$70,00')
+elif tamanho_cabelo >= 31 and tamanho_cabelo <= 50:
+    print('O valor é R$100,00')
+else:
+    print('Favor consultar a recepção')
+
+# Chaining
+if tamanho_cabelo <= 20:
+    print('O valor é R$50,00')
+elif 21 <= tamanho_cabelo <= 30:
+    print('O valor é R$70,00')
+elif 31 <= tamanho_cabelo <= 50:
+    print('O valor é R$100,00')
+else:
+    print('Favor consultar a recepção')
+
+
+
+
+
