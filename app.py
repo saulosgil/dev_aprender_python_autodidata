228958ac46f3fc756d7afce60cd67fec6c7a95b5
saulosgil <saulosgil@hotmail.com>
2023-12-17 16:40:17 -0300

    ordenar por meio de uma propriedade - itemgetter

--- app.py
+++ app.py
@@ -1305,11 +1305,101 @@ for indice,fruta in enumerate(frutas, 0):
     else:
         print(indice, fruta)
 
+# Ordenar por propriedades
+pessoas = [
+    {'nome':  'Jonh',
+     'idade': 32,
+     'nivel_acesso': 2},
+    {'nome':  'Carol',
+     'idade': 18,
+     'nivel_acesso': 3},
+    {'nome':  'Thomas',
+     'idade': 45,
+     'nivel_acesso': 5},
+    {'nome':  'Amanda',
+     'idade': 23,
+     'nivel_acesso': 1}
+]
 
+# Para ordernar por uma propriedade, podemos usar a fct itemgetter do módulo operator
+from operator import itemgetter
 
+pessoas.sort(key=itemgetter('nome')) # ordenando pela chave nome
+print(pessoas) 
 
+pessoas.sort(key=itemgetter('idade')) # ordenando pela chave idade
+print(pessoas) 
 
+# ordenando quando não há chaves
+from operator import itemgetter
 
+produtos = [
+    ('Celular', 750),
+    ('Bicicleta', 1500),
+    ('Microfone', 550)
+]
 
+produtos.sort(key=itemgetter(0)) # ordena pelo indice 0
+print(produtos)
 
+produtos.sort(key=itemgetter(1)) # ordena pelo indice 1
+print(produtos)
 
+# ordenando por uma matriz
+from operator import itemgetter
+
+matriz = [
+    [5, 10],
+    [15, 21],
+    [1, 5]
+]
+
+matriz.sort(key=itemgetter(0)) # ordena pelo primeiro indice
+print(matriz)
+
+matriz.sort(key=itemgetter(1)) # ordena pelo segundo indice
+print(matriz)
+
+# Lembrar que a fct sor permite devinir a ordem crescente ou descendente pelo args reverse
+matriz.sort(key=itemgetter(1),  reverse=True) # ordena pelo segundo indice
+print(matriz)
+
+# DESAFIOS 🥇
+# Ordene a lista de produtos abaixo pelo preço em ordem crescente
+produtos = [
+    {'nome': 'Celular',
+     'preco': 1500
+     },
+    {'nome': 'Monitor',
+     'preco': 500
+     },
+    {'nome': 'Microfone',
+     'preco': 300
+     }
+]
+
+from operator import itemgetter
+
+produtos.sort(key=itemgetter('preco'))
+print(produtos)
+
+# Ordene em ordem descrescente a lista de equipamento_filmagem por valor do 
+# equipamento
+equipamento_filmagem = [
+    ('Tripé', 300),
+    ('Câmera', 1700),
+    ('Iluminação', 200),
+]
+
+from operator import itemgetter
+
+equipamento_filmagem.sort(key=itemgetter(1), reverse=True)
+print(equipamento_filmagem)
+
+# Ordene em ordem crescente a cotacao_moedas com base no valor da moeda
+cotacao_moedas = [['usd', 5.25], ['brl', 1.56], ['eur', 6.47]]
+
+from operator import itemgetter
+
+cotacao_moedas.sort(key=itemgetter(1))
+print(cotacao_moedas)
