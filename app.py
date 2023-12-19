0b011362a55f1c5a5c60dcf3fa73ddbd9fe2374c
saulosgil <saulosgil@hotmail.com>
2023-12-19 13:34:42 -0300

    filter

--- app.py
+++ app.py
@@ -1445,3 +1445,51 @@ def extrair_cores(pinturas):
 # aplica fct na lista
 cores = list(map(extrair_cores, pinturas)) 
 print(cores) # imprime lista de cores
+
+# Como filtrar dados de uma colecao usando filter
+nomes = ['larissa', 'rafael', 'marcus','john']
+
+# função para pegar 'marcus' na lista
+def pessoa_aprovada(pessoa):
+    if pessoa == 'marcus':
+        return True
+    else:
+        return False
+
+print(list(filter(pessoa_aprovada, nomes)))
+print(list(map(pessoa_aprovada, nomes))) # aplica a fct em todos os elementos
+
+# Exemplos
+pinturas = [
+    ['Pintura Classica', 'Azul', 1857],
+    ['Pintura Medieval', 'Vermelha', 1867],
+    ['Pintura Moderna', 'Verde', 1897]
+]
+
+def eh_antiguidade(pintura):
+    if pintura[2] < 1890:
+        return True
+    else:
+        return False
+
+print(list(filter(eh_antiguidade, pinturas)))
+print(list(map(eh_antiguidade, pinturas)))
+
+# DESAFIO 1 🥇
+
+'''
+Usando a lista abaixo, filtre apenas as vagas com salário acima de R$2500
+'''
+vagas = [
+    ['vaga 1', 1200],
+    ['vaga 2', 2550],
+    ['vaga 3', 5000]
+]
+
+def salario_acima_2500(salario):
+    if salario[1] > 2500:
+        return True
+    else:
+        return False
+
+print(list(filter(salario_acima_2500, vagas)))
