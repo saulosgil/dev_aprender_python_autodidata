6b4d8b6b216c09f9fc27e8d50bc01791be632795
saulosgil <saulosgil@hotmail.com>
2024-02-26 09:14:10 -0300

    dict compreheension and readme-onde parei

--- README.md
+++ README.md
@@ -2,24 +2,4 @@
 
 This repository consists of scripts related to classes and exercises of the curso Python autodidata of the [Dev Aprender](https://devaprender.com/)
 
-Obs. Onde parei: Modulo 10 - aula 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Obs. Onde parei: Modulo 10 - aula 4
--- modulo_10/app.py
+++ modulo_10/app.py
@@ -75,7 +75,6 @@ def eh_numero_par(numero):
 print([i for i in range(20) if eh_numero_par(i)])
 
 # Desafio 1
-from random
 from pprint import pprint
 
 # Usando a lista compreheension, crie a seguinte lista:
@@ -107,11 +106,100 @@ deve ser como a lista a seguir:
 
 print([i + ' Pago' if i in pagamento_realizado else i + ' Devendo' for i in participantes])
 
+# Dictionary Compreheension
+'''
+Uma Dictionary Comprehension em Python é uma maneira concisa e eficiente de criar dicionários.
+Assim como as List Comprehensions, as Dictionary Comprehensions permitem criar dicionários
+em uma única linha de código de forma legível e eficiente.
+
+A sintaxe básica de uma Dictionary Comprehension é semelhante à List Comprehension, mas em
+vez de criar uma lista, você cria um dicionário.
+
+A sintaxe básica é:
+
+novo_dicionario = {chave: valor for item in iterável}
+
+onde:
+chave: É a chave do dicionário; 
+valor: É o valor associado à chave; 
+item: É a variável que representa cada elemento do iterável; 
+iterável: É a coleção de elementos que está sendo percorrida (por exemplo, uma lista, tupla,
+conjunto, ou até mesmo uma string)
+'''
+from random import randint
+from pprint import pprint
+
+pprint({i: i for i in range(20)})
+
+# ex1. Popular dicionarios
+produtos = ['produto1', 'produto2', 'produto3', 'produto4', 'produto5']
+
+pprint({produto: 1 for produto in produtos})
+
+# Gerar matriz de valores teste
+pprint({produto: [0 for i in range(5)] for produto in produtos})
+pprint({produto: [i*2 for i in range(5)] for produto in produtos})
+
+pprint({produto: [randint(1000,15000) for i in range(5)] for produto in produtos}) # com valores aleatórios
 
 
+# Desafios 🥇
+# Desafio 1 
 
+# Usando a lista seguir como base:
 
+'''
+sorteios = ['sorteio1','sorteio2','sorteio3']
 
+participantes = ['joel','jessica', 'maria','cris','Larissa', 'rafael', 'marcus', 'john']
+'''
+
+# crie a seguir, selecionando o ganhador aleatóriamente um nomes da lista de participantes. A ideia é simular quem irá ganhar cada sorteio, sua lista deve gerar a seguinte estrutura(porém o nome pode vir a ser diferente, já que estamos selecionando os nomes aleatóriamente)
+'''
+{
+
+    sorteio1: 'cris',
+
+    sorteio2: 'rafael',
+
+    sorteio3: 'marcus',
+
+}
+'''
+import random
+from pprint import pprint
+
+sorteios = ['sorteio1','sorteio2','sorteio3']
+participantes = ['joel','jessica', 'maria','cris','Larissa', 'rafael', 'marcus', 'john']
+
+pprint({sorteio: random.choice(participantes) for sorteio in sorteios})
+
+# Desafio 2
+'''
+Precisamos de dados de testes para criar contas temporárias, no momento precisamos de gerar 5
+valores de 1 a 100, e esses valores precisam Precisamos gerar 5 valores de 1 a 100 aleatóriamente.
+
+E estes valores precisam ser gerados para cada grupo na lista abaixo grupos 
+'''
+grupos = ['grupo 1', 'grupo 2', 'grupo 3']
+
+'''O resultado esperado é o dicionário com a estrutura a seguir(os valores entre contindos dentro da lista estarão diferentes, uma vez que os valores abaixo foram geradores aleatóriamente)
+
+{
+
+ 'grupo 1': [93, 97, 63, 36, 34],
+
+ 'grupo 2': [81, 24, 22, 46, 52],
+
+ 'grupo 3': [5, 35, 6, 86, 37]
+
+}
+'''
+import random
+from pprint import pprint
+
+grupos = ['grupo 1', 'grupo 2', 'grupo 3']
+pprint({grupo: [randint(1,101) for i in range(5)] for grupo in grupos})
 
 
 
